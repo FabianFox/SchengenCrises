fd3a265aa4fd1df22956f0c40cdb25545e0b11f0
FabianFox <fabianguelzau@web.de>
2021-03-18 18:57:17 +0100

    Migratory routes

--- code/2_Analysis_MigrationRoute.R
+++ code/2_Analysis_MigrationRoute.R
@@ -7,6 +7,9 @@ if (!require("xfun")) install.packages("xfun")
 pkg_attach2("tidyverse", "rio", "countrycode", "lubridate", "forcats",
             "patchwork", "janitor")
 
+# English locale (for this session)
+Sys.setlocale("LC_TIME", "English")
+
 # Theme
 ### ------------------------------------------------------------------------###
 # Additional settings (i.e. for figures)
@@ -22,7 +25,49 @@ theme.basic <- theme_minimal() +
 
 # Load data from https://frontex.europa.eu/assets/Migratory_routes/2021/Detections_of_IBC_2021_02_05.xlsx
 ### ------------------------------------------------------------------------###
-border_crossings.df <- import("https://frontex.europa.eu/assets/Migratory_routes/2021/Detections_of_IBC_2021_02_05.xlsx") %>%
+rfg.df <- import("https://frontex.europa.eu/assets/Migratory_routes/2021/Detections_of_IBC_2021_02_05.xlsx") %>%
   clean_names() %>%
   pivot_longer(cols = -c(1:3), names_to = "date", values_to = "number") %>%
-  mutate(date = format(parse_date_time(date, orders = c("m/Y")), "%m-%Y"))
+  mutate(
+    date = parse_date_time(date, orders = c("m/Y"), truncated = 1),
+    main_routes = case_when(
+      route %in% c("Black Sea Route", 
+                   "Circular Route from Albania to Greece",
+                   "Eastern Borders Route", 
+                   "Other", 
+                   "Western African Route",
+                   "Western Mediterranean Route") ~ "Other",
+      route == "Central Mediterranean Route" ~ "Central Mediterranean",
+      route == "Eastern Mediterranean Route" ~ "Eastern Mediterranean",
+      route == "Western Balkan Route" ~ "Western Balkan",
+      TRUE ~ NA_character_)) 
+
+# Aggregate arrivals by route and month
+rfg_agg.df <- rfg.df %>%
+  filter(year(date) > 2014) %>%
+  group_by(date = ymd(date), main_routes) %>%
+  summarise(number = sum(number)) %>%
+  ungroup()
+
+# Plot
+arrivals.fig <- ggplot(rfg_agg.df, aes(x = date, y = number, linetype = 
+                                         factor(main_routes, 
+                                                levels = c(
+                                                  "Central Mediterranean",
+                                                  "Eastern Mediterranean",
+                                                  "Western Balkan",
+                                                  "Other")))) +
+  geom_line(stat = "identity") +
+  scale_x_date(breaks = seq(as.Date("2015-01-01"), 
+                            as.Date("2022-06-01"), "year"),
+               date_labels = "%Y") +
+  scale_linetype_manual(values = c("dotted", "solid", "dashed", "dotdash")) +
+  labs(x = "", y = "", caption = "Data: Frontex (2021)", linetype = "Routes") +
+  theme.basic
+
+# Export plots
+### ------------------------------------------------------------------------###
+ggsave(
+  plot = arrivals.fig, "./figures/Fig 2 - Arrivals by route.tiff", 
+  width = 12, height = 6, unit = "in",  dpi = 300
+)
